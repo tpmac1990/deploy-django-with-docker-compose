b7ea107ebd9ce5c405928dc2e2b35a468ba9fbb6
tpmac1990 <terence@interaction.net.au>
2022-07-03 22:16:56 +1000

    added redis + celery

--- app/app/settings.py
+++ app/app/settings.py
@@ -137,3 +137,7 @@ STATIC_ROOT = '/vol/web/static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CELERY_BROKER_URL = "redis://redis:6379"
+CELERY_RESULT_BACKEND = "redis://redis:6379"
