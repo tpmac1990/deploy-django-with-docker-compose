e7843e6101f47bed69d3039225ad8c9b75d04285
tpmac1990 <terence@interaction.net.au>
2022-07-03 13:33:42 +1000

    WIP - added redis - run container failed

--- app/app/settings.py
+++ app/app/settings.py
@@ -137,3 +137,6 @@ STATIC_ROOT = '/vol/web/static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
